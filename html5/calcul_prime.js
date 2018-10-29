1b1ac8a669479025310796dce8246bb8c15f7420
Mihai Vanca <mihaisplus@gmail.com>
2018-10-29 11:34:17 +0200

    added stop for worker

--- html5/calcul_prime.js
+++ html5/calcul_prime.js
@@ -1,16 +1,33 @@
+var stopped = false;
 function is_prime(n){
-    for (var i = 2 ; i * i<= n; i++){
+    for (var i = 2 ; i * i <= n; i++){
         if( n % i == 0)
             return false;
     }
     return true;
 }
 
-
-for (var i=1e9; i < 1e10; i++){
-    if (is_prime(i)){
-        postMessage(i);
+onmessage() = function(e){
+    if (e.data == "stop"){
+        stopped = true;
     }
 }
 
-postMessage("gata");
+function calculeaza_prime(start_value_value){
+    if (stopped){
+
+    }  
+    else{
+        for (var i = start_value; i < start_value + 1000; i++){
+            if (is_prime(i))
+                postMessage(i);
+            
+        }
+        start_value += 1000;
+    }
+        if (start_value < 1e10)
+            setTimeout(calculeaza_prime, 1, start_value);
+        else
+            postMessage("gata");
+}
+
