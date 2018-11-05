53f8bd6527d16a31771e249ccbc38bdcea1cbd38
Mihai Vanca <mihaisplus@gmail.com>
2018-11-05 10:51:31 +0200

    added rotation p in angles bussiness version 2

--- html5/orientation.js
+++ html5/orientation.js
@@ -1,5 +1,5 @@
 window.addEventListener("deviceorientation", on_device_orientation);
-document.getElementById("id_bussiness_version").innerHTML = "Bussiness version: 2018.10.29.4";
+document.getElementById("id_bussiness_version").innerHTML = "Bussiness version: 2018.10.29.5";
 window.addEventListener("devicemotion", on_device_motion);
 var R = 10;
 function on_device_orientation(e){
@@ -22,10 +22,10 @@ function on_device_motion(e){
     var acc_y = e.accelerationIncludingGravity.y;
 
     document.getElementById("id_acc_z").innerHTML = acc_z.toFixed(2);
-    document.getElementById("id_acc_z").innerHTML = acc_x.toFixed(2);
-    document.getElementById("id_acc_z").innerHTML = acc_y.toFixed(2);
+    document.getElementById("id_acc_x").innerHTML = acc_x.toFixed(2);
+    document.getElementById("id_acc_y").innerHTML = acc_y.toFixed(2);
 
-    document.getElementById("id_rot_x").innerHTML = Math.atan(acc_x / acc_z) * 180 / Math.PI;
+    document.getElementById("id_rot_x").innerHTML = -Math.atan(acc_x / acc_z) * 180 / Math.PI;
     document.getElementById("id_rot_y").innerHTML = Math.atan(acc_y / acc_z) * 180 / Math.PI;
 }
 
