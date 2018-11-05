9bd3b4acd5d32c4be8eaf52da90878e132a48406
Mihai Vanca <mihaisplus@gmail.com>
2018-11-05 10:25:44 +0200

    added device accel

--- html5/orientation.js
+++ html5/orientation.js
@@ -1,5 +1,6 @@
 window.addEventListener("deviceorientation", on_device_orientation);
 document.getElementById("id_bussiness_version").innerHTML = "Bussiness version: 2018.10.29.1";
+window.addEventListener("devicemotion", on_device_motion);
 var R = 10;
 function on_device_orientation(e){
     document.getElementById("id_alpha").innerHTML = e.alpha;
@@ -14,3 +15,10 @@ function on_device_orientation(e){
     context.arc(canvas.width/2 + e.gamma / 90 * (canvas.width/2 - R), canvas.height/2 + e.beta/90 * (canvas.height /2 -R), R, 0, 2*Math.PI);
     context.stroke();
 }
+
+function on_device_motion(e){
+    document.getElementById("id_acc_z").innerHTML = e.accelerationIncludingGravity.z;
+    document.getElementById("id_acc_x").innerHTML = e.accelerationIncludingGravity.x;
+    document.getElementById("id_acc_y").innerHTML = e.accelerationIncludingGravity.y;
+}
+
