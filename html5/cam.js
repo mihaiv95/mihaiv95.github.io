2357fcc54bac0111383891ad273d7fe8bca92851
Mihai Vanca <mihaisplus@gmail.com>
2018-11-12 11:36:04 +0200

    Camera and promise example back camera with canvas

--- html5/cam.js
+++ html5/cam.js
@@ -1,7 +1,14 @@
 document.getElementById("id_bussiness_version").innerHTML = "Bussiness version: 2018.11.12.1";
-
 document.getElementById("id_button").addEventListener("click", start);
+document.getElementById("id_video").addEventListener("touchstart", on_touch);
+document.getElementById("id_video").addEventListener("mousedown", on_touch);
 
+function on_touch(e){
+    var canvas = document.getElementById("id_canvas");
+    var context = canvas.getContext("2d");
+    var video = document.getElementById("id_video");
+    context.drawImage(video, 0, 0);
+}
 
 function on_success(e){
     document.getElementById("id_video").srcObject = e;
@@ -15,7 +22,7 @@ function start(){
     var c = {
             audio: true, 
             video:{
-                facingMode: "environment"
+                facingMode: "user"
             }
         };
     navigator.mediaDevices.getUserMedia(c)
