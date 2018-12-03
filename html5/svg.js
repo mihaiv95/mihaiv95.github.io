308f48f6d7e69f3d99a416b50e58087fc12e97f1
Mihai Vanca <mihaisplus@gmail.com>
2018-12-03 12:47:51 +0200

    svg5

--- html5/svg.js
+++ html5/svg.js
@@ -1,5 +1,5 @@
 window.addEventListener("deviceorientation", on_device_orientation);
-document.getElementById("id_bussiness_version").innerHTML = "Bussiness version: 2018.12.3.3";
+document.getElementById("id_bussiness_version").innerHTML = "Bussiness version: 2018.12.3.4";
 // window.addEventListener("devicemotion", on_device_motion);
 var R = 10;
 function on_device_orientation(e){
@@ -7,8 +7,8 @@ function on_device_orientation(e){
     var circle = document.getElementById("id_circle");
     var R = 20;
 
-    var svg_width = svg.getAttribute("width")
-    var svg_height = svg.getAttribute("height")
+    var svg_width = svg.getAttribute("width");
+    var svg_height = svg.getAttribute("height");
 
     circle.setAttribute("cx", svg.width/2 + e.gamma / 90 * (svg.width/2 - R) + "px");
     circle.setAttribute("cy", svg.height/2 + e.beta / 90 * (svg.height/2 - R) + "px");
