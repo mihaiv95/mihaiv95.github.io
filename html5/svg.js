3c703396fa21d8f749d2e651fa7599830873ae3c
Mihai Vanca <mihaisplus@gmail.com>
2018-12-03 12:35:31 +0200

    svg3

--- html5/svg.js
+++ html5/svg.js
@@ -1,5 +1,5 @@
 window.addEventListener("deviceorientation", on_device_orientation);
-document.getElementById("id_bussiness_version").innerHTML = "Bussiness version: 2018.12.3.1";
+document.getElementById("id_bussiness_version").innerHTML = "Bussiness version: 2018.12.3.2";
 // window.addEventListener("devicemotion", on_device_motion);
 var R = 10;
 function on_device_orientation(e){
@@ -7,6 +7,6 @@ function on_device_orientation(e){
     var circle = document.getElementById("id_circle");
     var R = 20;
 
-    circle.setAttribute("cx", svg.width/2 + e.gamma / 90 * (svg.width/2 - R), svg.height/2 + e.beta/90 * (svg.height /2 -R) + "px");
-    circle.setAttribute("cy", svg.width/2 + e.gamma / 90 * (svg.width/2 - R), svg.height/2 + e.beta/90 * (svg.height /2 -R)+ "px");
+    circle.setAttribute("cx", svg.width/2 + e.gamma / 90 * (svg.width/2 - R) + "px");
+    circle.setAttribute("cy", svg.height/2 + e.beta / 90 * (svg.height/2 - R) + "px");
 }
